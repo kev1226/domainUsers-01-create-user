5a31f334dc08e77e3feeefb3b2eb534fb6590cc9
kev1226 <kevin_alt12@hotmail.com>
2025-07-06 00:17:55 -0500

    test roles

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,11 +1,16 @@
-import { Controller, Post, Body } from '@nestjs/common';
+import { Controller, Post, Body, UseGuards } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
+import { AuthGuard } from '../common/enums/guards/auth.guard';
+import { Roles } from 'src/common/enums/decorators/auth.decorator';
+import { Role } from 'src/common/enums/rol.enum';
 
 @Controller('create')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
+  @UseGuards(AuthGuard)
+  @Roles(Role.ADMIN)
   @Post()
   create(@Body() createUserDto: CreateUserDto) {
     return this.usersService.create(createUserDto);
